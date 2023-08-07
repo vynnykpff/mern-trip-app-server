ded12ad7cb71eb416a91dd0a821824a51df203bc
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-08-08 01:06:56 +0300

    update: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,10 @@
-import { UsersException } from './users.exception';
-import { User, UserDocument, UserEditableFields } from './models/user.model';
-import { Types } from 'mongoose';
-import { PatchUserDTO } from './dtos/patch-user.dto';
-import { isImageUrl } from '@/utils/isImageUrl';
-import { UserToken } from './models/user-token.model';
-import { UserDTO } from './dtos/user.dto';
+import {UsersException} from './users.exception';
+import {User, UserDocument, UserEditableFields} from './models/user.model';
+import {Types} from 'mongoose';
+import {PatchUserDTO} from './dtos/patch-user.dto';
+import {isImageUrl} from '@/utils/isImageUrl';
+import {UserToken} from './models/user-token.model';
+import {UserDTO} from './dtos/user.dto';
 
 export class UsersService {
 	static async getUserById(userId: string | Types.ObjectId) {
@@ -19,8 +19,8 @@ export class UsersService {
 	}
 
 	static async patchUser(user: UserDocument, patchUserDto: PatchUserDTO) {
-		const { username, avatar } = patchUserDto;
-		if (username && (await User.findOne({ username }))) {
+		const {username, avatar} = patchUserDto;
+		if (username && (await User.findOne({username}))) {
 			throw UsersException.UserAlreadyExist();
 		}
 
@@ -40,7 +40,7 @@ export class UsersService {
 
 	static async deleteUser(user: UserDocument) {
 		await user.delete();
-		await UserToken.deleteOne({ userId: user.id });
+		await UserToken.deleteOne({userId: user.id});
 		return UserDTO.createDto(user);
 	}
 }
